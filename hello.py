9b7de548a9f0482995643ecebec2061063471886
gbehera <ranjan.kbr@gmail.com>
2018-07-02 22:26:53 +0530

    Sample File to print Hello World

---
+++ hello.py
@@ -0,0 +1,10 @@
+#Welcome to Python
+
+print ("Hello World")
+
+#Single Line comment starts with # in Python
+'''
+This is a Multi Line comment 
+it was awesome Intepreter. This line Interprets a Multi Line comment
+'''
+print (" It works Good")
